5f26cbfe570dcd6bc19f9e00e2db7622fcf751de
Eoniz <nathan.artisien@gmail.com>
2019-02-28 18:42:40 +0100

    Command and Component class logic

---
+++ src/structures/command.js
@@ -0,0 +1,41 @@
+import Discord from 'discord.js';
+import Component from './component';
+
+/**
+ * Abstract Command class. 
+ * It defines all the logic behind a command
+ */
+class Command {
+    
+    /**
+     * Command constructor
+     * @param {string} name The !command name
+     * @param {Component} component The component associated to the command 
+     */
+    constructor(name, component) {
+        this.name = name;
+        this.component = component;
+    }
+
+    /**
+     * Function trying to parse the command. If it's the right command,
+     * it will execute the command.
+     * 
+     * @param {Discord.Client} client The discord bot client 
+     * @param {Discord.Guild} guild The discord bot guild
+     * @param {Discord.Message} msg The original message sent
+     * @param {string} cmd The command typed
+     * @param {string[]} args The args passed on the command
+     * @returns {boolean} True if command is executed. False otherwise
+     */
+    parse(client, guild, msg, cmd, args) {
+        if(cmd !== this.name)
+            return false;
+        
+        this.component.componentWillExecute(client, guild, msg, cmd, args);
+        this.component.execute(client, guild, msg, cmd, args);
+        this.component.componentDidExecute(client, guild, msg, cmd, args);
+    }
+}
+
+module.exports = Command;
---
+++ src/structures/component.js
@@ -0,0 +1,47 @@
+import Command from './command';
+
+class Component {
+
+    /**
+     * Component constructor
+     * @param {Command} command The command associated to this component 
+     */
+    constructor(command) {
+        this.command = command;
+    }
+
+    /**
+     * Function called juste before executing the command
+     * 
+     * @param {Discord.Client} client The discord bot client 
+     * @param {Discord.Guild} guild The discord bot guild
+     * @param {Discord.Message} msg The original message sent
+     * @param {string} cmd The command typed
+     * @param {string[]} args The args passed on the command
+     */
+    componentWillExecute(client, guild, msg, cmd, args) {}
+    
+    /**
+     * Function to execute the command logic
+     * 
+     * @param {Discord.Client} client The discord bot client 
+     * @param {Discord.Guild} guild The discord bot guild
+     * @param {Discord.Message} msg The original message sent
+     * @param {string} cmd The command typed
+     * @param {string[]} args The args passed on the command
+     */
+    execute(client, guild, msg, cmd, args) {}
+
+    /**
+     * Function called juste after executing the command
+     * 
+     * @param {Discord.Client} client The discord bot client 
+     * @param {Discord.Guild} guild The discord bot guild
+     * @param {Discord.Message} msg The original message sent
+     * @param {string} cmd The command typed
+     * @param {string[]} args The args passed on the command
+     */
+    componentDidExecute(client, guild, msg, cmd, args) {}
+}
+
+module.exports = Component;
