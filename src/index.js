a1591c93cd1ccd9e375af3752d31bcc7c737a2f5
Eoniz <nathan.artisien@gmail.com>
2019-02-28 18:51:27 +0100

    Added basic discorded login

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import Discord from 'discord.js';
+
+class Discorded {
+    
+    /**
+     * Discorded lib constructor
+     * @param {string} authToken The discord bot token
+     * @param {boolean} autoLogin Auto log discord bot ?
+     * @param {Discord.ClientOptions} options The discord client options 
+     */
+    constructor(authToken, autoLogin = true, options = null) {
+        this._authToken = authToken;
+        this.client = new Discord.Client(options);
+
+        this._init(autoLogin);
+    }
+
+    /**
+     * Init discored app
+     * @param {boolean} autoLogin Auto log discord bot ? 
+     */
+    _init(autoLogin) {
+        this.client.on('ready', (evt) => {
+            console.log(`[Discorded] Connected, logged in as : ${this.client.user.tag} (${this.client.user.id})`);
+        });
+
+        this.client.on('message', (msg) => {
+            console.log(`[Discored] New message sent on channel ${msg.channel.name} (${msg.content})`);
+        })
+
+        if(autoLogin) {
+            this.login();
+        }
+    }
+
+    /**
+     * Function to log in discord bot
+     */
+    login() {
+        this.client.login(true);
+    }
+}
+
+module.exports = Discorded;
